6029462da577fdb84a049fafd40e340df03b07dd
SenTech-666 <prointeres07@gmail.com>
2025-11-25 18:08:41 +1000

    убраны лишние функции

--- public/homePage.js
+++ public/homePage.js
@@ -1,11 +1,9 @@
 'use strict';
 
-
 const logoutButton     = new LogoutButton();
 const ratesBoard       = new RatesBoard();
 const moneyManager     = new MoneyManager();
 const favoritesWidget  = new FavoritesWidget();
-
 
 const updateRates = () => {
     ApiConnector.getStocks(resp => {
@@ -16,41 +14,39 @@ const updateRates = () => {
     });
 };
 
+
+const updateFavoritesFromData = (data) => {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(data);
+    moneyManager.updateUsersList(data); 
+};
+
 const updateFavorites = () => {
     ApiConnector.getFavorites(resp => {
         if (resp.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(resp.data);
-            moneyManager.updateUsersList(resp.data);
+            updateFavoritesFromData(resp.data);
         }
     });
 };
 
 
-const refreshRatesAndFavorites = () => {
-    updateRates();
-    updateFavorites();
-};
-
-
 logoutButton.action = () => ApiConnector.logout(resp => resp.success && location.reload());
-
 
 ApiConnector.current(resp => {
     if (resp.success) ProfileWidget.showProfile(resp.data);
 });
+
 updateRates();
 updateFavorites();
-setInterval(updateRates, 60000);
+setInterval(updateRates, 60000);  
 
 
 const handleMoneyOperation = (response, successMessage) => {
     if (response.success) {
-        
         ProfileWidget.showProfile(response.data);
         
-       
-        refreshRatesAndFavorites();
+      
+        updateRates(); 
         
         moneyManager.setMessage(true, successMessage);
     } else {
@@ -58,32 +54,29 @@ const handleMoneyOperation = (response, successMessage) => {
     }
 };
 
-// Пополнение
+
 moneyManager.addMoneyCallback = data => {
     ApiConnector.addMoney(data, resp => 
         handleMoneyOperation(resp, 'Баланс успешно пополнен!')
     );
 };
 
-// Конвертация
 moneyManager.conversionMoneyCallback = data => {
     ApiConnector.convertMoney(data, resp => 
         handleMoneyOperation(resp, 'Конвертация выполнена успешно!')
     );
 };
 
-// Перевод
 moneyManager.sendMoneyCallback = data => {
     ApiConnector.transferMoney(data, resp => 
         handleMoneyOperation(resp, 'Перевод выполнен успешно!')
     );
 };
 
-/* ===== 6. Избранное ===== */
 favoritesWidget.addUserCallback = userData => {
     ApiConnector.addUserToFavorites(userData, resp => {
         if (resp.success) {
-            refreshRatesAndFavorites();
+            updateFavoritesFromData(resp.data);
             favoritesWidget.setMessage(true, 'Пользователь добавлен в избранное');
         } else {
             favoritesWidget.setMessage(false, resp.error);
@@ -94,7 +87,7 @@ favoritesWidget.addUserCallback = userData => {
 favoritesWidget.removeUserCallback = userId => {
     ApiConnector.removeUserFromFavorites({ id: userId }, resp => {
         if (resp.success) {
-            refreshRatesAndFavorites();
+            updateFavoritesFromData(resp.data);
             favoritesWidget.setMessage(true, 'Пользователь удалён из избранного');
         } else {
             favoritesWidget.setMessage(false, resp.error);
